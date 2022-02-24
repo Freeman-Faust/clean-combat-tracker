f63f9b731b5be32199cad78218c4feaba61ec249
Freeman-Faust <freemanfaust@hotmail.com>
2022-02-24 09:42:51 -0400

    Working on displaying initiative order

--- src/components/dispInitOrd.js
+++ src/components/dispInitOrd.js
@@ -3,13 +3,39 @@ import InitOrd from './initOrd';
 
 function DispInitOrd(props)
 {
+
+    function calcDetInt(dex)
+    {
+        return(parseInt(dex) + 22);
+        //todo: make the actual calculation
+        //Math.floor(Math.random()*(max-min+1))+parseInt(dex)
+    }
+    
+    const initList = [];
+    for(const comb in props.comb)
+    {
+        let tmpObj = {};
+        tmpObj.name = comb.name;
+        tmpObj.detInt = calcDetInt(comb.dex);
+        initList.push(tmpObj);
+    }
+
+    console.log(initList);
+
+    const sortedInitList = initList.sort((a,b) => a.detInt - b.detInt); // do some work to sort this by detInt 
+        
+    console.log(sortedInitList);
+        
+        
+
+
    
 return(
     <div id="dispInit">
-        <button onClick={InitOrd}>Calculate Initiative</button>
+        <button onClick={handleClick}>Calculate Initiative</button>
         <div>
-        {props.comb.map(comb=>{
-            return(<InitOrd name={comb.name} DEX_mod={comb.DEX_mod}/>)
+        {props.sortedInitList.map(sortedComb=>{
+            return(<InitOrd sortedComb={sortedComb}/>)
         })}
         </div>
     </div>
